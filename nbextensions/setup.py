330603479d7c9c0ecfac29b6dd63b24eeb22e901
Sebastian Stigler <sebastian@sebastian-stigler.de>
2015-05-03 17:27:20 +0200

    improve the install process with default stat of nbextensions

--- nbextensions/setup.py
+++ nbextensions/setup.py
@@ -13,36 +13,36 @@ def install(extension, use_symlink=False, profile='default', enable=True):
     install_nbextension(ext_dir, symlink=use_symlink,
                         overwrite=use_symlink, user=True)
 
-    if enable:
-        # Enable the extension in the given profile.
-        profile_dir = locate_profile(profile)
-        cm = ConfigManager(profile_dir=profile_dir)
-        cm.update('notebook', {"load_extensions": {extension + "/main": True}})
+    # Enable/Disable the extension in the given profile.
+    profile_dir = locate_profile(profile)
+    cm = ConfigManager(profile_dir=profile_dir)
+    cm.update('notebook', {"load_extensions": {extension + "/main": enable}})
 
 
 def main():
     import argparse
     import sys
+    import yaml
 
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title='subcommands',
                                        description='valid subcommands',
                                        help='additional help')
-
     install_parser = subparsers.add_parser('install')
     install_parser.add_argument('--develop', action='store_true',
                                 help=("Install extensions  as a "
                                       "symlink to the source."))
-    install_parser.add_argument('--no-enable', action='store_true',
-                                help="Install but don't enable the extension.")
     install_parser.add_argument('--profile', action='store', default='default',
                                 help=("The name of the profile to use."))
+
+    with open(os.path.join(base_dir, 'default_state.yaml')) as fil:
+        default_state = yaml.load(fil)
 
     args = parser.parse_args(sys.argv[1:])
     for a, dirs, b in os.walk(base_dir):
         for ext in dirs:
             install(ext, use_symlink=args.develop, profile=args.profile,
-            enable=(not args.no_enable))
+            enable=default_state.get(ext, True))
         break
 
 if __name__ == '__main__':
